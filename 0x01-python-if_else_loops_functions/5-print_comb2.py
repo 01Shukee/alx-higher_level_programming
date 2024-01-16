992482189a0e800119eab485f3f1fa1acecd8890
Shukee <chidubemifeanyi40@gmail.com>
2024-01-16 21:12:44 +0100

    if else loop project

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for number in range(100):
-    if number == 99:
-        print(f"{number:02d}", end="\n")
+for i in range(100):
+    if i < 99:
+        print("{:02d}, ".format(i), end="")
     else:
-        print(f"{number:02d}, ", end='' )
+        print("{:02d}".format(i))
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range(1, 10):
-        if j > i:
-            if i != 8:
-                print(f"{i}{j}, ", end="")
-            if i == 8:
-                    print(f"{i}{j}", "\n")
+        if i >= j:
+            continue
+        if i == 8 and j == 9:
+            print("{:d}{:d}".format(i, j))
+        else:
+            print("{:d}{:d}".format(i, j), end=", ")
