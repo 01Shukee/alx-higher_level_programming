554d7f14d8a164fe60be09120ee01cee310ff610
Shukee <chidubemifeanyi40@gmail.com>
2024-01-18 14:36:48 +0100

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,18 +1,17 @@
-#!/usr/bin/env python3
-import argparse
-
+#!/usr/bin/python3
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Prints the number of command line arguments and their values.")
-    parser.add_argument("args", nargs="*", help="List of command line arguments.")
-    args = parser.parse_args()
-
-    num_args = len(args.args)
-    if num_args == 0:
-        print(f"{num_args} arguments.")
-    elif num_args == 1:
-        print(f"{num_args} argument:")
+    import sys
+    i = len(sys.argv) - 1
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
     else:
-        print(f"{num_args} arguments:")
+        print("{} arguments:".format(i))
 
-    for i, arg in enumerate(args.args, start=1):
-        print(f"{i}: {arg}")
+    if i >= 1:
+        i = 0
+        for arg in sys.argv:
+            if i != 0:
+                print("{}: {}".format(i, arg))
+            i += 1
